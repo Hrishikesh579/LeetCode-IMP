170e1bf00759795f88d45bdb07b86d77b6373067
Hrishikesh Warpe <141823939+Hrishikesh579@users.noreply.github.com>
2026-09-20 16:16:27 +0530

    Time: 27 ms (39.58%) | Memory: 41.9 MB (20.09%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        vector<string> v, vEven;
+        int n = s.size();
+        for(int i = 0; i < n; i++){
+            int a = i-1, b = i+1;
+            while(a >= 0 && b < n){
+                if(s[a] != s[b]) break;
+                a--;
+                b++;
+            }
+            a++, 
+            b--;
+            v.push_back(s.substr(a, (b-a+1)));
+        }
+        int maxCount = INT_MIN, maxIndex = -1;
+        int m = v.size();
+        for(int i = 0; i < m; i++){
+            int temp = v[i].size();
+            maxCount = max(maxCount, temp);
+            if(maxCount == temp) maxIndex = i;
+        }
+        for(int i = 0; i < n; i++){
+            int a = i, b = i+1;
+            while(a >= 0 && b < n){
+                if(s[a] != s[b]) break;
+                a--;
+                b++;
+            }
+            a++;
+            b--;
+            vEven.push_back(s.substr(a, (b-a+1)));
+            a = i-1, b = i;
+            while(a >= 0 && b < n){
+                if(s[a] != s[b]) break;
+                a--;
+                b++;
+            }
+            a++;
+            b--;
+            vEven.push_back(s.substr(a, (b-a+1)));
+        }
+        int maxCount2 = INT_MIN, maxIndex2 = -1;
+        int o = vEven.size();
+        for(int i = 0; i < o; i++){
+            int temp = vEven[i].size();
+            maxCount2 = max(maxCount2, temp);
+            if(maxCount2 == temp) maxIndex2 = i;
+        }
+        // if(maxIndex == -1 && maxIndex2 == -1) return "";
+        if(maxIndex == -1 && maxIndex2 != -1) return vEven[maxIndex2];
+        else if(maxIndex != -1 && maxIndex == -1) return v[maxIndex];
+        else if(maxIndex != -1 && maxIndex != -1){
+            int maximum = max(maxCount, maxCount2);
+            if(maximum == maxCount) return v[maxIndex];
+            else return vEven[maxIndex2];
+        }
+        return "";
+    }
+};
