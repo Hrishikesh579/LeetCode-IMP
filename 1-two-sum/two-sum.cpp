0cccac07211dfb58f05a16cecdbdf261fe17d57e
Hrishikesh Warpe <141823939+Hrishikesh579@users.noreply.github.com>
2026-09-23 16:19:30 +0530

    Time: 7 ms (42.41%) | Memory: 15.9 MB (5.88%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> um;
+        vector<int> res;
+        for(int i = 0; i < nums.size(); i++){
+            if(um.find(target - nums[i]) != um.end()) {
+                res.push_back(um[target - nums[i]]);
+                res.push_back(i);
+            }
+            um[nums[i]] = i;
+        }
+        return res;
+    }
+};
