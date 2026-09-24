3ef5d46359b58ec5ee3a6c151fe9a964af1ff219
Hrishikesh Warpe <141823939+Hrishikesh579@users.noreply.github.com>
2026-09-24 12:17:22 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (99.82%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,44 +1,53 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        // vector<vector<int>> res;
-        // intervals.push_back(newInterval);
-        // sort(intervals.begin(), intervals.end(), [&](vector<int>& a, vector<int>& b){
-        //     return a[0] < b[0];
-        // });
+        //-----------------------------------------------------Dummy Element Approach--------------------------------------------------------
         // int n = intervals.size();
+        // int i = n-1;
+        // intervals.push_back({100001, 100001});
+        // while(i >= 0 && intervals[i][0] > newInterval[0]) {
+        //     intervals[i+1] = intervals[i];
+        //     i--;
+        // }
+        // vector<vector<int>> res;
+        // intervals[i+1] = newInterval;
         // int lowerLimit = intervals[0][0], upperLimit = intervals[0][1];
-        // for(int i = 0; i < n; i++){
+        // for(i = 0; i < n+1; i++){
         //     if(intervals[i][0] <= upperLimit){
         //         upperLimit = max(upperLimit, intervals[i][1]);
         //     } else{
         //         res.push_back({lowerLimit, upperLimit});
         //         lowerLimit = intervals[i][0];
-        //         upperLimit = intervals[i][1];
+        //         upperLimit = intervals[i][1];  
         //     }
         // }
         // res.push_back({lowerLimit, upperLimit});
         // return res;
-        int n = intervals.size();
-        int i = n-1;
-        intervals.push_back({100000, 100000});
-        while(i >= 0 && intervals[i][0] > newInterval[0]) {
-            intervals[i+1] = intervals[i];
-            i--;
-        }
+        
+        // On The Fly approach
         vector<vector<int>> res;
-        intervals[i+1] = newInterval;
-        int lowerLimit = intervals[0][0], upperLimit = intervals[0][1];
-        for(i = 0; i < n+1; i++){
-            if(intervals[i][0] <= upperLimit){
-                upperLimit = max(upperLimit, intervals[i][1]);
-            } else{
-                res.push_back({lowerLimit, upperLimit});
-                lowerLimit = intervals[i][0];
-                upperLimit = intervals[i][1];  
-            }
+        int i = 0, n = intervals.size();
+
+        // 1. Add all intervals that come completely before the new interval
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            res.push_back(intervals[i]);
+            i++;
         }
-        res.push_back({lowerLimit, upperLimit});
+
+        // 2. Merge all overlapping intervals with newInterval
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        res.push_back(newInterval); // Add the merged new interval
+
+        // 3. Add all remaining intervals that come after
+        while (i < n) {
+            res.push_back(intervals[i]);
+            i++;
+        }
+
         return res;
     }
 };
