ae9f23ff2980e06872b7609f548324af2381b433
Hrishikesh Warpe <141823939+Hrishikesh579@users.noreply.github.com>
2026-09-26 15:53:55 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (52.10%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string res = "";
-        for(auto& i : s){
-            int temp = static_cast<int>(i);
-            if((temp >= 65 && temp <= 90) || (temp >= 97 && temp  <= 122) || (temp >= 48 && temp <= 57)){
-                res += i;
-            }
-        }
-        transform(res.begin(), res.end(), res.begin(), [](char c){
-            return static_cast<char>(tolower(c));
-        });
-        int n = res.size();
-        for(int i = 0; i < n/2; i++){
-            if(res[i] != res[n-1-i]) return false;
+        // transform(s.begin(), s.end(), s.begin(), [](unsigned char c){
+        //     return tolower(c);
+        // });
+        int n = s.size();
+        int i = 0, j = n-1;
+        while(i < j){
+            while(i < j && !isalnum(static_cast<unsigned char>(s[i]))) i++;
+            while(i < j && !isalnum(static_cast<unsigned char>(s[j]))) j--;
+            if(tolower(static_cast<unsigned char>(s[i])) != tolower(static_cast<unsigned char>(s[j]))) return false;
+            i++;
+            j--;
         }
         return true;
     }
