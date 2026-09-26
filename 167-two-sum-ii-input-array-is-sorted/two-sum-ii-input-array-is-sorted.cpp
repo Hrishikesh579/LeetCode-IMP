df01c6f7b65b04571174f43d13f4bf4c3b3a7cab
Hrishikesh Warpe <141823939+Hrishikesh579@users.noreply.github.com>
2026-09-26 15:34:38 +0530

    Time: 3 ms (10.10%) | Memory: 25.5 MB (15.34%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
-    int BinarySearch(vector<int>& nums, int left, int right, int target){
-        if(left > right) return -1;
-        int mid = left + (right - left)/2;
+    // int BinarySearch(vector<int>& nums, int left, int right, int target) {
+        //------------------------------------------------------------Binary
+        //Search O(nlogn) approach--------------------------------------------
+        //     if(left > right) return -1;
+        //     int mid = left + (right - left)/2;
 
-        if(nums[mid] == target) return mid;
+        //     if(nums[mid] == target) return mid;
 
-        if(nums[mid] > target) return BinarySearch(nums, left, mid - 1, target);
+        //     if(nums[mid] > target) return BinarySearch(nums, left, mid - 1,
+        //     target);
 
-        return BinarySearch(nums, mid + 1, right, target);
-    }
-    vector<int> twoSum(vector<int>& numbers, int target) {
+        //     return BinarySearch(nums, mid + 1, right, target);
+        // }
+        vector<int> twoSum(vector<int>& numbers, int target) {
+        //     vector<int> res;
+        //     int n = numbers.size();
+        //     for(int i = 0; i < n; i++){
+        //         int temp = BinarySearch(numbers, i+1, n-1, target -
+        //         numbers[i]); if(temp != -1){
+        //             res.push_back(i+1);
+        //             res.push_back(temp + 1);
+        //         }
+        //     }
+        //     return res;
+
+        //-------------------------------------------------------------------O(n)
+        //approach-------------------------------------------------------
         vector<int> res;
         int n = numbers.size();
-        for(int i = 0; i < n; i++){
-            int temp = BinarySearch(numbers, i+1, n-1, target - numbers[i]);
-            if(temp != -1){
-                res.push_back(i+1);
-                res.push_back(temp + 1);
+        int i = 0, j = n - 1;
+        while (i < j) {
+            if((numbers[i] + numbers[j]) > target) j--;
+            else if((numbers[i] + numbers[j]) < target) i++;
+            else {
+                res.insert(res.end(), {i+1, j+1});
+                break;
             }
         }
         return res;
